b9b20203f12f6c42cd2f9db9b176a4b3de727416
Jay Khatri <khatri.florida@gmail.com>
2017-02-05 13:03:39 -0500

    added resources with python modules and added a template for adding and deleting a given mongo instance

--- test.py
+++ test.py
@@ -1,20 +1,33 @@
-from person import Person
-from flask import Flask
+from resources import Person
+from flask import Flask, render_template, request, redirect
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 
-#note: in flask-pymongo, the name of this file must be the same name as the databse at hand, in this case, test.
 
 """
 note: in mongo, a document is a an object or a row, and a collection is a table
 a column represents a key in the databse and values are analgous
+
+note: in flask-pymongo, the name of this file must be the same name as the databse at hand, in this case, test.
 """
+
+b = Person()
 app = Flask(__name__) 
 mongo = PyMongo(app)
 
 @app.route('/')
 def home_page():
-    return str(mongo.db.sites.find_one_or_404({}))
+    return render_template('render.html')
+
+@app.route("/add", methods=['POST'])
+def addh():
+    print "hello"
+    if request.method == 'POST':
+        return redirect('render.html')
+
+@app.route('/delete/', methods=['POST'])
+def delete():
+    pass
 
 if __name__ == "__main__":
     app.run()
