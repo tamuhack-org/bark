3f9641e7057dc407a3a5d33160894da01b28a282
Jay Khatri <khatri.florida@gmail.com>
2017-02-05 20:10:58 -0600

    basic adding/deleting functionality, once needs are established, we can continue

--- test.py
+++ test.py
@@ -10,9 +10,8 @@ a column represents a key in the databse and values are analgous
 
 note: in flask-pymongo, the name of this file must be the same name as the databse at hand, in this case, test.
 """
-#creating a person instance
-b = Person()
-
+#creating a person instance 
+b = Person() 
 app = Flask(__name__) 
 mongo = PyMongo(app)
 
@@ -20,19 +19,26 @@ mongo = PyMongo(app)
 def home_page():
     #we can get info from a mongo document
     print str(mongo.db.sites.find_one({}))
-    return render_template('render.html')
+    count = mongo.db.sites.count({})
+    return render_template('render.html', count=count)
 
 #testing a post method from the form called "render.html" (this is still in progress)
 @app.route("/add", methods=['POST'])
-def addh():
-    print "hello"
-    if request.method == 'POST':
-        return redirect('render.html')
+def add():
+    fname = request.form['fname-add']
+    lname = request.form['lname-add']
+    email = request.form['email-add']
+    mongo.db.sites.insert({"first_name": fname, "last_name": lname, "email": email})
+    count = mongo.db.sites.count({})
+    return render_template('render.html', count=count)
 
-@app.route('/delete/', methods=['POST'])
+@app.route('/delete', methods=['POST'])
 def delete():
-    pass
-
+    b = request.form['fname-delete']
+    mongo.db.sites.delete_one({"first_name": b})
+    count = mongo.db.sites.count({})
+    return render_template('render.html', count=count)
+            
 if __name__ == "__main__":
     app.run()
 
