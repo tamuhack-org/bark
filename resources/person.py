ea94e761f2719fd6965fa67917b69bd3260abc0a
deniseirvin <dairvin9@tamu.edu>
2017-02-08 22:00:49 -0600

    improved person.py

--- resources/person.py
+++ resources/person.py
@@ -1,24 +1,27 @@
 class Person:
-    def __init__(self, first_name = None):
-        self.fname = first_name
-        self.lname = None
-        self.email = None
-
-    def set_first(self, first):
-        self.fname = first
-
-    def set_last(self, last):
-        self.lname = last
-
-    def set_email(self, email):
-        self.email = email
-            
+    def __init__(self, *initial_data, **kwargs): 
+    
+    #^ we are gonna wanna use kwargs.
+    #^ http://stackoverflow.com/questions/2466191/set-attributes-from-dictionary-in-python 
+    #^ if you dont know what im talking about
+    
+        for dictionary in initial_data:
+            for key in dictionary:
+                setattr(self, key, dictionary[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+    # once we have a full list of all the attributes finalized we can add 
+    # a safeguard that makes sure attributes that werent set are set to None.
+    # We honestly dont even have to do that bc we are using NoSQL. But if we 
+    # want to standardized all the objects we can. 
+    
     def toDict(self):
         #returns a dictionsary with ifnromation for a given person
-        return {"fname": self.fname, "lname": self.lname, "email": self.email}
+        # self.dict already does this. (we can delete this function but i left it in to show you)
+        return self.__dict__
 
-    def fromDict(self, dic):
+    def updateAttributes(self, dic):
+        # I changed the name bc I thought "fromDict" didnt explain the purpose very well.
         #updates a given person based on an inputted dictionary
-        self.fname = dic["fname"]
-        self.lname = dic["lname"]
-        self.email = dic["email"]
+        for key in dic:
+            setattr(self, key, dic[key]) 
