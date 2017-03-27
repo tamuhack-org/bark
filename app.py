75866b9a5aa3cead126f3a61648258dc27517581
Jay Khatri <khatri.florida@gmail.com>
2017-03-26 22:59:43 -0500

    changed the parsing algorithms slightly, with recent suggestions

--- app.py
+++ app.py
@@ -10,12 +10,11 @@ app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://tamuhack17:Tamuhackdb17@ds129600.mlab.com:29600/m_engine_db'
 }
 
-#things that need to be added
+# things that need to be added
 #   1: error handling for manual upload of an entry
 #   2: pagination on results page
 #   3: Back buttons on all pages to get back home?
 #   4: loading html div for upload pages
-
 
 m_person.db.init_app(app)
 Person = m_person.Person
@@ -25,7 +24,7 @@ parser = typeform.Typeform_Parser(Person)
 def home_page():
     # we can get info from a mongo document
     count = Person.objects.count()
-    return render_template('home.html', count = count)
+    return render_template('home.html', count=count)
 
 
 # route that handles buttons on the home page
@@ -45,7 +44,7 @@ def modify():
             return render_template('add_delete.html', count=count)
         elif request.form['action'] == 'delete':
             query = request.form['fname_delete']
-            to_delete = Person.objects(first_name = query)
+            to_delete = Person.objects(first_name=query)
             to_delete.delete()
             count = Person.objects.count()
             return render_template('add_delete.html', count=count)
@@ -57,33 +56,32 @@ def modify():
 def participants():
     count = Person.objects.count()
     entries = Person.objects()
-    #if the something is sent via the action argument (happens only with an upload)
+    # if the something is sent via the action argument (happens only with an upload)
     action = request.args.get('action', '')
     if action:
         if action == 'uploaded':
             parser.save_data()
-            # m_person.save_update(parser.get_data())
             count = Person.objects.count()
             entries = Person.objects()
-            return render_template('results.html', count=count, entries=entries, msg = "Successful Upload")
+            return render_template('results.html', count=count, entries=entries, msg="Successful Upload")
         elif action == 'return':
-            return render_template('results.html', count=count, entries=entries, msg = "You didn't upload")
-    #otherwise, its a query or an empty query
+            return render_template('results.html', count=count, entries=entries, msg="You didn't upload")
+    # otherwise, its a query or an empty query
     else:
-        #second value is a default argument ''
+        # second value is a default argument ''
         query = request.args.get('q', '')
         if query:
-            entries = Person.objects(Q(first_name__icontains=query)|Q(last_name__icontains=query)|Q(email__icontains=query))
-        return render_template('results.html', count=count, entries=entries, query=query, msg = "randomshit")
+            entries = Person.objects(Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(email__icontains=query))
+        return render_template('results.html', count=count, entries=entries, query=query, msg="test message")
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
     if request.method == 'GET':
-        #loads data into the parser
+        # loads data into the parser
         count = Person.objects.count()
-        sample = str(parser.get_preview())
-        print sample
-        count_diff = parser.get_entry_count()
+        sample = str(parser.parse_preview())
+        count_diff = 4
+        #count_diff = parser.get_entry_count()
         return render_template('upload.html', count=count, sample_data=sample ,count_diff=count_diff, msg="For uploading data from a request")
 
 if __name__ == "__main__":
--- resources/typeform.py
+++ resources/typeform.py
@@ -1,13 +1,11 @@
 import requests
-import json
 import re
 from random import randint
-from math import *
-import flask_mongoengine
 
+DEFAULT_REQUEST_LIMIT = 1000
 
 class Typeform_Parser(object):
-    def __init__(self, p_class):
+    def __init__(self, person_class):
         self.schema = {"28125773": "first_name", "28125775": "last_name",
                       "28125781": "gender", "28127706": "travel",
                       "28127225": "additional", "28125780": "experience",
@@ -16,64 +14,75 @@ class Typeform_Parser(object):
                       "28128125":"school", "28158536": "resume"}
         self.request_string = "https://api.typeform.com/v1/form/HDv04s?key=598bae62949ccf0f2098d86db19592d0aa0a2260"
         self.data = []
-        self.person_db = p_class
-        #initialize the attributes of the person object
+        self.person_db = person_class
+        # initialize the attributes of the person object
 
     def save_data(self):
-        data_list = self._return_data("data")
+        data_list = self.parse_data()
         for person_data in data_list:
             try:
-                #if the person object exists with a given email, ignore it
+                # if the person object exists with a given email, ignore it
                 self.person_db.objects.get(email=person_data["email"])
                 print person_data["email"]
             except Exception:
-                #if we get a "does not exist", create a new person
+                # if we get a "does not exist", create a new person
                 p = self.person_db()
                 for x in person_data:
                     setattr(p, x, person_data[x])
                 p.save()
 
-    def _return_data(self, preview):
+    def parse_preview(self):
+        # gets a python dictionary object from a get request
+        data = requests.get(self.request_string).json()
+        responses = data["responses"]
+        total_count = data["stats"]["responses"]["total"]
+        #return nothing if the count is 0
+        if not total_count:
+            return None
+        # defines a limit for generating a random number
+        limit = min(total_count, DEFAULT_REQUEST_LIMIT)
+        #define an output, and continue to query random indexes until the output has a "non-empty" entry
+        output_data = None
+        while not output_data:
+            output_data = responses[randint(0, limit)]["answers"]
+            print output_data
+        return output_data
+
+    def parse_data(self):
         output = []
-        num_entries = self.get_entry_count()
-        upper = 1000
+        num_entries = self._get_metadata()["total"]
         if not num_entries:
             return None
-        if preview is not "preview":
-            upper = int(ceil(num_entries*.001)*1000)
-        for request_range in range(0, upper, 1000):
-            r = requests.get(self.request_string+"&offset="+str(request_range))
-            dict = json.loads(r.text)
-            responses = dict["responses"]
-            #responses may be empty, so we check here
-            for x in responses:
-                #the answers section is the part that we actually want
-                entry = x["answers"]
-                #if the entry part is full (Not full means that the application wasn't finished)
-                if entry:
-                    #create a new dictionary
-                    curr_dic = {}
-                    for x,y in entry.iteritems():
-                        #check to see if the value has something in it
-                        if y:
-                            #get rid of all
-                            x = re.sub("[^0-9]", "", x)
-                            #if there exists a key in schema that is a part of our current key
-                            if x in self.schema:
-                                curr_dic[self.schema[x]] = y
-                    output.append(curr_dic)
-        if preview is "preview":
-            return output[randint(0,len(output))]
+        #define a limit
+        limit = min(num_entries, DEFAULT_REQUEST_LIMIT)
+        #number of iterations or paginated requests
+        iter = num_entries/limit + 1
+        for request_range in range(0, iter):
+            # mult request range by 1000 and make it an offset in the typeform request for each iteration
+            responses = requests.get(self.request_string +"&offset=" + str(request_range * DEFAULT_REQUEST_LIMIT)).json()["responses"]
+            #if the number of entries is less than 1000
+            if responses:
+                for x in responses:
+                    # the answers section is the part that we actually want
+                    entry = x["answers"]
+                    # if the entry part is full (Not full means that the application wasn't finished)
+                    if entry:
+                        # create a new dictionary
+                        curr_dic = {}
+                        for x,y in entry.iteritems():
+                            # check to see if the value has something in it
+                            if y:
+                                # get rid of all
+                                x = re.sub("[^0-9]", "", x)
+                                # if there exists a key in schema that is a part of our current key
+                                if x in self.schema:
+                                    curr_dic[self.schema[x]] = y
+                        output.append(curr_dic)
         return output
 
-    def get_preview(self):
-        return self._return_data("preview")
 
-    def get_data(self):
-        return self._return_data("data")
-
-    def get_entry_count(self):
+    #this method returns simple metadata for a the first page of a typeform request:
+    #keywords are showing, total and completed
+    def _get_metadata(self):
         r = requests.get(self.request_string)
-        dict = json.loads(r.text)
-        count = dict["stats"]["responses"]["total"]
-        return int(count)
+        return r.json()["stats"]["responses"]
